a4ea181dd1cc5bd4a0dffd7721f8a572ebeea6ae
Boris Villazon-Terrazas <bvillazon@fi.upm.es>
2011-07-20 19:12:51 +0100

    Included more test cases

---
+++ D013-1table3columns2rows1nullvalue/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Person (
+ID integer,
+Name varchar(50),
+DateOfBirth varchar(50)
+);
+INSERT INTO Person (ID, Name, DateOfBirth) VALUES (1,"Alice", NULL);
+INSERT INTO Person (ID, Name, DateOfBirth) VALUES (2,"Bob", "September, 2010");
