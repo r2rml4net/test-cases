0ba93a2ca339aa4b6f16632a933c7984a22482cc
Boris Villazón-Terrazas <boris.villazon@terrazas.name>
2011-08-29 22:43:50 +0100

    Fixing database 12

--- D012-2tables2duplicates0nulls/create.sql
+++ D012-2tables2duplicates0nulls/create.sql
@@ -10,6 +10,6 @@ CREATE TABLE Lives (
       fname CHAR(20),
       lname CHAR(20),
       city CHAR(20));
-INSERT INTO IOUs (fname, lname, amount) VALUES ('Bob', 'Smith', 'London');
-INSERT INTO IOUs (fname, lname, amount) VALUES ('Sue', 'Jones', 'Madrid');
-INSERT INTO IOUs (fname, lname, amount) VALUES ('Bob', 'Smith', 'London');
+INSERT INTO Lives (fname, lname, city) VALUES ('Bob', 'Smith', 'London');
+INSERT INTO Lives (fname, lname, city) VALUES ('Sue', 'Jones', 'Madrid');
+INSERT INTO Lives (fname, lname, city) VALUES ('Bob', 'Smith', 'London');
