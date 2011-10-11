0ecda7b3c28e244a4ed4a0938a59efac35b98a53
Boris Villazón-Terrazas <boris.villazon@terrazas.name>
2011-10-11 02:07:03 +0200

    Remove the set of files

--- D002-1table2columns1row/create.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Student (
-ID integer,
-Name varchar(50)
-);
-INSERT INTO Student (ID, Name) VALUES(10,'Venus');
--- D003-1table3columns1row/create.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Student (
-ID integer,
-FirstName varchar(50),
-LastName varchar(50)
-);
-INSERT INTO Student (ID, FirstName, LastName) VALUES (10,'Venus', 'Williams');
