10788a51776383032d734b14e001e8726b05037d
Boris Villazon-Terrazas <bvillazon@fi.upm.es>
2012-02-15 02:14:57 +0100

    Internal cleaning

--- D010-1table1primarykey3colums3rows/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "Country Info" (
-  "Country Code" INTEGER PRIMARY KEY,
-  "Name" CHAR(100),
-  "ISO 3166" CHAR(10)
-);
-INSERT INTO "Country Info" ("Country Code", "Name", "ISO 3166") VALUES (1, 'Bolivia, Plurinational State of', 'BO');
-INSERT INTO "Country Info" ("Country Code", "Name", "ISO 3166") VALUES (2, 'Ireland', 'IE');
-INSERT INTO "Country Info" ("Country Code", "Name", "ISO 3166") VALUES (3, 'Saint Martin (French part)', 'MF');
