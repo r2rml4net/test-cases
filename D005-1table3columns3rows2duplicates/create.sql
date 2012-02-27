a3948b358ab0adf0a189206683603245ef0dba84
Boris Villazon-Terrazas <bvillazon@fi.upm.es>
2012-02-27 18:27:47 +0100

    Replace CHAR for VARCHAR

--- D005-1table3columns3rows2duplicates/create.sql
+++ D005-1table3columns3rows2duplicates/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "IOUs" (
-      "fname" CHAR(20),
-      "lname"  CHAR(20),
+      "fname" VARCHAR(20),
+      "lname"  VARCHAR(20),
       "amount" FLOAT);
 INSERT INTO "IOUs" ("fname", "lname", "amount") VALUES ('Bob', 'Smith', 30);
 INSERT INTO "IOUs" ("fname", "lname", "amount") VALUES ('Sue', 'Jones', 20);
--- D010-1table1primarykey3colums3rows/create.sql
+++ D010-1table1primarykey3colums3rows/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Country Info" (
   "Country Code" INTEGER PRIMARY KEY,
-  "Name" CHAR(100),
-  "ISO 3166" CHAR(10)
+  "Name" VARCHAR(100),
+  "ISO 3166" VARCHAR(10)
 );
 INSERT INTO "Country Info" ("Country Code", "Name", "ISO 3166") VALUES (1, 'Bolivia, Plurinational State of', 'BO');
 INSERT INTO "Country Info" ("Country Code", "Name", "ISO 3166") VALUES (2, 'Ireland', 'IE');
