f2ec3e8da5a70fef27b4fce704a4032b39923168
Boris Villazon-Terrazas <bvillazon@fi.upm.es>
2012-03-13 02:30:47 +0100

    Update TCs according to Richard comments III

--- D003-1table3columns1row/create.sql
+++ D003-1table3columns1row/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Student" (
-"ID" integer,
-"FirstName" varchar(50),
-"LastName" varchar(50)
+"ID" INTEGER,
+"FirstName" VARCHAR(50),
+"LastName" VARCHAR(50)
 );
 INSERT INTO "Student" ("ID", "FirstName", "LastName") VALUES (10,'Venus', 'Williams');
