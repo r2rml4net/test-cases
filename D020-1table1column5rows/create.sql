02675b81517786de1030f2aca51fd69c19aefec2
Boris Villazon-Terrazas <bvillazon@fi.upm.es>
2012-08-10 02:12:13 +0200

    Include example.com as common base on the tcs

--- D020-1table1column5rows/create.sql
+++ D020-1table1column5rows/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE "Student" (
 "Name" VARCHAR(50)
 );
 
-INSERT INTO "Student" ("Name") VALUES ('http://company.com/Alice');
+INSERT INTO "Student" ("Name") VALUES ('http://example.com/company/Alice');
 INSERT INTO "Student" ("Name") VALUES ('Bob');
 INSERT INTO "Student" ("Name") VALUES ('Bob/Charles');
 INSERT INTO "Student" ("Name") VALUES ('path/../Danny');
